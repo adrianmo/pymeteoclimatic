e699766282d1a6f09a452bac68b7f5e9ae510073
Adrián Moreno <adrian@morenomartinez.com>
2020-06-18 11:45:05 +0200

    Bump version to 0.0.6
    Signed-off-by: Adrián Moreno <adrian@morenomartinez.com>

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='pymeteoclimatic',
-    version='0.0.5',
+    version='0.0.6',
     description='A Python wrapper around the Meteoclimatic service',
     long_description=long_description,
     long_description_content_type='text/markdown',
